adcf1c574f5635acc89bd278db00e48125499501
Yusuke Nakamura <yusuke1994525@gmail.com>
2021-04-01 18:41:42 +0900

    bundle gem s3_cache_store

---
+++ lib/s3_cache_store.rb
@@ -0,0 +1,6 @@
+require "s3_cache_store/version"
+
+module S3CacheStore
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/s3_cache_store/version.rb
@@ -0,0 +1,3 @@
+module S3CacheStore
+  VERSION = "0.1.0"
+end
